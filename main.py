58e7657a1ed4efa0d8b8a5acd0ff6b3b1d29d814
StudentJulian <105376777+StudentJulian@users.noreply.github.com>
2022-05-13 23:02:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -139,9 +139,11 @@ row2_col2.subheader("Welche konkreten Maßnahmen können helfen?")
 #Variablen-Bedeutung -> Hier erstmal vom Kurs rauskopiert, Zugang zu model und X_train muss noch erledigt werden
 def plot_variable_importance(model, X_train):
     imp=DataFrame({"imp":model.feature_importances_, "names":X_train.columns}).sort_values("imp", ascending=True)
-    fig, ax = plt.subplots(figsize=(imp.shape[0]/6,imp.shape[0]/5), dpi=300)
+    fig2, ax = plt.subplots(figsize=(imp.shape[0]/6,imp.shape[0]/5), dpi=300)
     ax.barh(imp["names"],imp["imp"], color="#93c47d") 
     ax.set_xlabel('\nBedeutung der Variablen')
     ax.set_ylabel('Features\n') 
     ax.set_title('Bedeutung der Variablen - Abbildung\n') 
     plt.show() 
+
+row2_col1.pyplot(fig2, use_container_width = True)
