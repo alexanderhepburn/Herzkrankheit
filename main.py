ebd00502351dcd29fb8c6b4e96cb53f7821fd215
StudentJulian <105376777+StudentJulian@users.noreply.github.com>
2022-05-13 21:06:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -95,6 +95,7 @@ fig1, ax = plt.subplots(figsize = (8, 4))
 ax.bar(1, berechneHeartDisease(), color = barcolor(berechneHeartDisease))
 ax.set_ylabel("Risiko [%]")
 plt.yticks([0, 0.25, 0.5, 1])
+plt.xticks()
 row1_col1.pyplot(fig1, use_container_width = True)
 
 
