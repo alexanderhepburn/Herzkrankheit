ee86934c1f1d09856a592bb21f881e2a5e1b01ff
StudentJulian <105376777+StudentJulian@users.noreply.github.com>
2022-05-14 09:55:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -146,4 +146,4 @@ def plot_variable_importance(model, X_train):
     ax.set_title('Bedeutung der Variablen - Abbildung\n') 
     plt.show() 
 
-row2_col1.pyplot(plot_variable_importance(model, X_train), use_container_width = True) #Führt zu Fehler, muss nochmal schauen, woher der kommt
+row2_col1.pyplot(plot_variable_importance(model, X_train), use_container_width = True).head(5) #Führt zu Fehler, muss nochmal schauen, woher der kommt
