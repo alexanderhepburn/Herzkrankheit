516fe4ff359a4b02616328a1c2baf47944cffbfe
StudentJulian <105376777+StudentJulian@users.noreply.github.com>
2022-05-17 19:08:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -136,19 +136,20 @@ row2_col1.subheader("Welche 5 Faktoren ausgenommen des Alters sind am wichtigste
 row2_col2.subheader("Welche konkreten Maßnahmen können helfen?")
 # hier dachte ich daran, Nachrichten zu generieren, wie etwa: Gesunde Ernährung, Sport, weniger Rauchen etc.
 
-
+Imp = berechneHeartDisease().importance_getter
+row2_col1.write(Imp, use_container_width = True)
 
 
 
 #Variablen-Bedeutung -> Hier erstmal vom Kurs rauskopiert (4 - Trees, Forests, Ensembles)
 
-def plot_variable_importance(model, X_train):
-    imp=DataFrame({"imp":model.feature_importances_, "names":X_train.columns}).sort_values("imp", ascending=True)
-    fig2, ax = plt.subplots(figsize=(imp.shape[0]/6,imp.shape[0]/5), dpi=300)
-    ax.barh(imp["names"],imp["imp"], color="#93c47d") 
-    ax.set_xlabel('\nBedeutung der Variablen')
-    ax.set_ylabel('Features\n') 
-    ax.set_title('Bedeutung der Variablen - Abbildung\n') 
-    plt.show()
+#def plot_variable_importance(model, X_train):
+    #imp=DataFrame({"imp":model.feature_importances_, "names":X_train.columns}).sort_values("imp", ascending=True)
+    #fig2, ax = plt.subplots(figsize=(imp.shape[0]/6,imp.shape[0]/5), dpi=300)
+    #ax.barh(imp["names"],imp["imp"], color="#93c47d") 
+    #ax.set_xlabel('\nBedeutung der Variablen')
+    #ax.set_ylabel('Features\n') 
+    #ax.set_title('Bedeutung der Variablen - Abbildung\n') 
+    #plt.show()
 
-row2_col1.pyplot.barh(plot_variable_importance(model, X_train), use_container_width = True)
+#row2_col1.pyplot.barh(plot_variable_importance(model, X_train), use_container_width = True)
