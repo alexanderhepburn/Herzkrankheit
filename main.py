24eaff97143520ffecef51cf252a567d2fd02d65
StudentJulian <105376777+StudentJulian@users.noreply.github.com>
2022-05-19 21:08:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -94,14 +94,10 @@ def barcolor(berechneHeartDisease):
     return colorcode
 
 #Plot
-for label in (ax.get_xticklabels() + ax.get_yticklabels()):
-	label.set_fontsize(16)
-
-
 fig1, ax = plt.subplots(figsize = (8, 4))
 ax.bar(1, berechneHeartDisease(), color = barcolor(berechneHeartDisease))
 ax.set_ylabel("Risiko") #Ich hab das [%] rausgenommen, da das sonst missverstanden werden kann wenn yticks unter 1 sind -T
-plt.yticks([0, 0.25, 0.5, 0.75, 1])
+plt.yticks([0, 0.25, 0.5, 0.75, 1], labelsize = 16)
 plt.xticks([])
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
