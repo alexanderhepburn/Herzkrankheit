5dcc38571eeb955b7c0690264587c0eea133bf29
StudentJulian <105376777+StudentJulian@users.noreply.github.com>
2022-05-19 20:37:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -622,9 +622,9 @@ def farbederbars(list_of_all_risk_factors):
     for i in list_of_all_risk_factors:
         if i < 0:
             colorbar = '#93c47d'
-        elif 0 <= i < 0.02:
+        elif 0 <= i < 0.2:
             colorbar = '#ffd966'
-        elif 0.02 <= i < 0.05:
+        elif 0.2 <= i < 0.5:
             colorbar = '#f6b26b'
         else:
             colorbar = '#cc4125'
