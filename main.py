cc790788e77d24458fa01bc534aa3416694a5929
StudentJulian <105376777+StudentJulian@users.noreply.github.com>
2022-05-20 14:48:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -708,23 +708,23 @@ row5_col1.subheader("Hier können Sie Ihre Resultate downloaden")
 #Funktion
 Excel_contents = [
 'Faktor', 'Anteil am Risiko'
-'BMI', berechneRisikoVonBMI()
-'Rauchen', berechneRisikoVonSmoking()
-'Alkoholkonsum', berechneRisikoVonAlkohol()
-'Schlaganfall', berechneRisikoVonStroke()
-'Körperliche Gesundheit', berechneRisikoVonPhysicalHealth()
-'Mentale Gesundheit', berechneRisikoVonMentalHealth()
-'Gehschwierigkeiten', berechneRisikoVonDiffWalking()
-'Geschlecht', berechneRisikoVonSex()
-'Alter', berechneRisikoVonAgeCategory()
-'Ethnie', berechneRisikoVonRace()
-'Diabetes', berechneRisikoVonDiabetic()
-'Sport', berechneRisikoVonSport()
-'Generelles Wohlbefinden', berechneRisikoVonGenHealth()
-'Schlaf', berechneRisikoVonSchlaf()
-'Asthma', berechneRisikoVonAsthma()
-'Nierenkrankheiten', berechneRisikoVonKidneyDisease()
-'Hautkrebs', berechneRisikoVonSkinCancer()
+'BMI', berechneRisikoVonBMI(),
+'Rauchen', berechneRisikoVonSmoking(),
+'Alkoholkonsum', berechneRisikoVonAlkohol(),
+'Schlaganfall', berechneRisikoVonStroke(),
+'Körperliche Gesundheit', berechneRisikoVonPhysicalHealth(),
+'Mentale Gesundheit', berechneRisikoVonMentalHealth(),
+'Gehschwierigkeiten', berechneRisikoVonDiffWalking(),
+'Geschlecht', berechneRisikoVonSex(),
+'Alter', berechneRisikoVonAgeCategory(),
+'Ethnie', berechneRisikoVonRace(),
+'Diabetes', berechneRisikoVonDiabetic(),
+'Sport', berechneRisikoVonSport(),
+'Generelles Wohlbefinden', berechneRisikoVonGenHealth(),
+'Schlaf', berechneRisikoVonSchlaf(),
+'Asthma', berechneRisikoVonAsthma(),
+'Nierenkrankheiten', berechneRisikoVonKidneyDisease(),
+'Hautkrebs', berechneRisikoVonSkinCancer(),
 ]
 
 
