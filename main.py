f023d248f84b58b6653b20dc29a8589a44e915d5
GreatTobes <105391206+GreatTobes@users.noreply.github.com>
2022-05-22 02:13:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -680,7 +680,7 @@ row3_col1.subheader("Wo befindet sich Ihr Risiko im Vergleich?")
 
 def Ranking_Function():
    z = berechneHeartDisease()
-   x = Log_Reg.predict_proba(xTest)
+   x = LogisticRegression().predict_proba(xTest)
    x = x.copy() 
 #  x = x.values.tolist()
 #  solution = []
